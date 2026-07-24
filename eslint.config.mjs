c67062a53d6ed78e01b862fdeba46fae0d337bd9
Alain Cao Van Truong <cvt.alain@gmail.com>
2026-07-24 19:13:07 +0200

    chore(eslint): update rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,34 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  prettierConfig,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: {
+      "prettier/prettier": "error",
+
+      "react/jsx-first-prop-new-line": ["error", "multiline-multiprop"],
+      "react/jsx-max-props-per-line": ["error", { maximum: 1, when: "multiline" }],
+      "react/jsx-closing-bracket-location": ["error", "line-aligned"],
+
+      "import/order": [
+        "error",
+        {
+          groups: [["builtin", "external"], "internal"],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
